978f96ec5912f771d2d6ddbfe6bf60906ca785c3
Urs Joss <urs.joss@gmx.ch>
2021-01-25 23:11:46 +0100

    Update license headers

--- 03_kotlin_gradle_kotlin_dsl/subprojects/project2/src/main/kotlin/org/kordamp/sample/HelloWorld1.kt
+++ 03_kotlin_gradle_kotlin_dsl/subprojects/project2/src/main/kotlin/org/kordamp/sample/HelloWorld1.kt
@@ -1,7 +1,7 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  *
- * Copyright 2020 Urs Joss.
+ * Copyright 2021 Urs Joss.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
